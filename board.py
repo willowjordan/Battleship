66f896fc3ce050e92c9919ccc3f8cfe1bda731e7
willowjordan <willowtjordan@gmail.com>
2025-03-09 22:05:07 -0400

    - defined board and space class - removed piece file

---
+++ board.py
@@ -0,0 +1,16 @@
+class Space():
+    def __init__(self, occupied=False, shot=0):
+        self.occupied = occupied # True if occupied by a ship (this will only be true for primary grid)
+        self.shot = shot # 0 if not shot yet, -1 if miss, 1 if hit
+
+class Board():
+    def __init__(self, ships=None):
+        self.primaryGrid = {}
+        self.targetingGrid = {}
+
+        if ships is not None:
+            for s in ships:
+                pass # populate primary grid
+    
+    def fireShot(self, other, pos):
+        pass
