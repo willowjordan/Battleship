f6cfaf65d96c133df4b21bc108281ddea9b94de1
willowjordan <willowtjordan@gmail.com>
2025-03-10 15:49:23 -0400

    - added "host a lobby" screen - deleted unnecessary files

--- main.py
+++ main.py
@@ -1,44 +1,96 @@
 import tkinter as tk
-import pygame
 
-# container representing the main interactive area (primary grid and targeting grid)
-class GameScreen():
+class TitleScreen(tk.Frame):
     def __init__(self, master):
+        super().__init__()
         self.master = master
 
+        PAD = 10
+        BUTTONWIDTH = 40
+        BUTTONHEIGHT = 7
+
+        self.label = tk.Label(self, text="Battleship", font=('Arial', 18))
+        self.hostbutton = tk.Button(self, command=self.host_screen, text="Host Game", width=BUTTONWIDTH, height=BUTTONHEIGHT)
+        self.joinbutton = tk.Button(self, command=self.master.join_screen, text="Join Game", width=BUTTONWIDTH, height=BUTTONHEIGHT)
+        self.exitbutton = tk.Button(self, command=self.master.destroy, text="Quit", width=BUTTONWIDTH, height=BUTTONHEIGHT)
+
+        self.label.pack(pady=PAD)
+        self.hostbutton.pack(pady=PAD)
+        self.joinbutton.pack(pady=PAD)
+        self.exitbutton.pack(pady=PAD)
+    
+    def host_screen(self):
+        self.master.next_frame = HostScreen(self.master)
+        self.master.display_next()
+
+class HostScreen(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+
+        PAD = 10
+        BUTTONWIDTH = 40
+        BUTTONHEIGHT = 7
+
+        self.label = tk.Label(self, text="Host a Game...", font=('Arial', 18))
+        self.lobbynamelabel = tk.Label(self, text="Lobby Name: ")
+        self.lobbyname = tk.Text(self, height=3, bg="white", fg="black")
+        self.displaynamelabel = tk.Label(self, text="Display Name: ")
+        self.displayname = tk.Text(self, height=3, bg="white", fg="black")
+        self.startbutton = tk.Button(self, command=self.createLobby, text="Start Lobby", width=BUTTONWIDTH, height=BUTTONHEIGHT)
+
+        self.label.grid(row=1, column=1, columnspan=2)
+        self.lobbynamelabel.grid(row=2, column=1)
+        self.lobbyname.grid(row=2, column=2)
+        self.displaynamelabel.grid(row=3, column=1)
+        self.displayname.grid(row=3, column=2)
+        self.startbutton.grid(row=4, column=1, columnspan=2)
+    
+    def createLobby(self):
+        pass
+
+class JoinScreen(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+
+# container representing the main interactive area (primary grid and targeting grid)
+class GameScreen(tk.Frame):
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+
+# main game object
 class Game(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry("500x500")
         self.title("Battleship")
 
-        self.title_screen()
+        self.current_frame = None # current frame being displayed
+        self.next_frame = TitleScreen(self) # next frame to be displayed
+        self.display_next()
 
-    # display the title screen
     def title_screen(self):
-        PAD = 10
-        BUTTONWIDTH = 40
-        BUTTONHEIGHT = 7
+        ts = TitleScreen(self)
+        ts.pack()
 
-        root = tk.Tk()
-        root.geometry("500x500")
-        root.title("Battleship Title Screen")
-        frame = tk.Frame(root)
-        
-        label = tk.Label(frame, text="Battleship", font=('Arial', 18))
-        hostbutton = tk.Button(frame, command=self.host_screen, text="Host Game", width=BUTTONWIDTH, height=BUTTONHEIGHT)
-        joinbutton = tk.Button(frame, text="Join Game", width=BUTTONWIDTH, height=BUTTONHEIGHT)
-        exitbutton = tk.Button(frame, text="Quit", width=BUTTONWIDTH, height=BUTTONHEIGHT)
-
-        label.pack(pady=PAD)
-        hostbutton.pack(pady=PAD)
-        joinbutton.pack(pady=PAD)
-        exitbutton.pack(pady=PAD)
-
-        frame.pack()
-    
     def host_screen(self):
-        pass
+        hs = HostScreen(self)
+        hs.pack()
+
+    def join_screen(self):
+        js = JoinScreen(self)
+        js.pack()
+
+    # destroy current frame and display next
+    def display_next(self):
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+        self.current_frame = self.next_frame
+        self.current_frame.pack()
+        self.next_frame = None
+
 
 
 if __name__ == "__main__":
